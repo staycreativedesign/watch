d57703749eef773bbfebe3e20548545ff5093b0d
Gus P <gustavoanalytics@gmail.com>
2024-07-16 16:57:44 -0400

    + bg login + tailwindcss-animatecss - tailwindcss-animated - cleanup
    Added config.routes = false to clearance as prescribed by gem to set
    correct routes
    
    Removed application css as tailwind.application.css is the main css file
    also removed stylesheet  link to application.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,28 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  resources :passwords,
+            controller: 'clearance/passwords',
+            only: %i[create new]
+
+  resource :session,
+           controller: 'sessions',
+           only: [:create]
+
+  resources :accounts,
+            controller: 'clearance/users',
+            only: Clearance.configuration.user_actions do
+    resource :password,
+             controller: 'clearance/passwords',
+             only: %i[edit update]
+  end
+
+  resources :accounts, onoy: %i[new create edit update show]
+
+  get 'register' => 'accounts#new', as: 'sign_up'
+  get '/sign-in' => 'clearance/sessions#new', as: 'sign_in'
+  delete '/sign-out' => 'clearance/sessions#destroy', as: 'sign_out'
+
   # Defines the root path route ("/")
   root 'pages#index'
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
+    animatedSettings: {
+      animatedSpeed: 1000,
+      heartBeatSpeed: 500,
+      hingeSpeed: 2000,
+      bounceInSpeed: 750,
+      bounceOutSpeed: 750,
+      animationDelaySpeed: 500,
+      classes: ['bounce', 'heartBeat', 'fadeInDown', 'fadeOutUp']
+    },
     extend: {
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
@@ -23,7 +32,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-    require('tailwindcss-animated'),
+    require('tailwindcss-animatecss'),
     require('flowbite/plugin')
   ]
 }
