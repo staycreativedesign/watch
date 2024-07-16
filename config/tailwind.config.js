b9aade7c0b9c2b4b23d252336e963f29167fd9d5
Gus P <gustavoanalytics@gmail.com>
2024-07-16 13:02:54 -0400

    + tailwind animate package
    Added this package to enable animations a lot easier for tailwind
    https://www.tailwindcss-animated.com/configurator.html

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
@@ -19,6 +23,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    require('tailwindcss-animated'),
     require('flowbite/plugin')
   ]
 }
