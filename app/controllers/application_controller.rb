ddf81ca37062593f0f30c5b995f6e540e404fb7f
Gus P <gustavoanalytics@gmail.com>
2024-07-18 22:25:38 -0400

    + flash message
    making it easer ont he eyes to always render as turbo stream until its
    needed for somehting else

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
 
-  def flash_message(type:, messages:)
+  def flash_message(type:, messages: nil)
     render turbo_stream: turbo_stream.update('flash-messages', partial: 'shared/flash',
-                                                               locals: { type: type, messages: messages })
+                                                               locals: { type:, messages: })
   end
 end
