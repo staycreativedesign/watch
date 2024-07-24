b5e7fd4a3750f0b065783714b7848a7cf0dc317d
Gus P <gustavoanalytics@gmail.com>
2024-07-24 09:43:00 -0400

    + flash message for streams
    Flash streams added to helper to make it faster to code. flash frames
    have there own as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Clearance::Controller
 
   def flash_message(type:, messages: nil)
+    flash.now[type.to_sym] = messages
     render turbo_stream: turbo_stream.update('flash-messages', partial: 'shared/flash',
                                                                locals: { type:, messages: })
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def flash_type?(type)
-    if type == 'alert'
+    if type == 'error'
       'error-msg'
     else
       'success-msg'
     end
+  end
+
+  def render_flash_stream(type: 'success', messages: nil)
+    turbo_stream.update('flash-messages', partial: 'shared/flash',
+                                          locals: { type: })
   end
 end
